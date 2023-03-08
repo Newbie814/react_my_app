b4388a2436be9517b93cd65bef02d5faec0e2d59
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-08 15:38:48 -0500

    different ways to implement css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 const BookList = () => {
   return (
-    <section>
+    <section className='booklist'>
       <Book />
       <Book />
       <Book />
@@ -16,22 +16,36 @@ const BookList = () => {
 
 const Book = () => {
   return (
-    <article>
+    <article className='book'>
       <Image />
       <Title />
       <Author />
     </article>
   );
 };
-
+// styling options
 const Image = () => (
+  // external css
   <img
     src='https://m.media-amazon.com/images/I/51jayVNubpL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
     alt='Book Cover'
   />
 );
-const Title = () => <h2>How to Catch a Leprechaun</h2>;
-const Author = () => <h2>Adam Wallace</h2>;
+// css object
+const inLineHeadingStyles = {
+  color: '#617d98',
+  fontSize: '0.75rem',
+  marginTop: '0.5rem',
+};
+const Title = () => (
+  <h2 style={{ inLineHeadingStyles }}>How to Catch a Leprechaun</h2>
+);
+// inline css
+const Author = () => (
+  <h2 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+    Adam Wallace
+  </h2>
+);
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
