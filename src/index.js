9f96ef7831ab31d2a0baa2233f6a63eab8f31231
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-07 22:59:10 -0500

    basic style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './index.css';
+
 const BookList = () => {
   return (
     <section>
@@ -22,9 +24,14 @@ const Book = () => {
   );
 };
 
-const Image = () => <h2>image placeholder</h2>;
-const Title = () => <h2>Book Title</h2>;
-const Author = () => <h2>Author Name</h2>;
+const Image = () => (
+  <img
+    src='https://m.media-amazon.com/images/I/51jayVNubpL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
+    alt='Book Cover'
+  />
+);
+const Title = () => <h2>How to Catch a Leprechaun</h2>;
+const Author = () => <h2>Adam Wallace</h2>;
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
